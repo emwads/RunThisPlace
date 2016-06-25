e7e548cfdc35a477e4f63e086e1bdf6c9f25dcb1
emwads <iamwatiel@gmail.com>
2016-06-24 22:59:29 -0700

    first commit

---
+++ frontend/run_this_place.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+document.addEventListener("DOMContentLoaded",  () => {
+  ReactDOM.render(<div>RunThisPlace!!</div>, document.getElementById('content'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/run_this_place.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
