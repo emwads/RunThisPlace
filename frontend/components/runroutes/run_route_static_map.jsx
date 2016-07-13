d978f9d3213103324b7dfdcdc4c25e7651d6aec2
emwads <iamwatiel@gmail.com>
2016-07-13 00:09:21 -0700

    mapfix

--- frontend/components/runroutes/run_route_static_map.jsx
+++ frontend/components/runroutes/run_route_static_map.jsx
@@ -14,7 +14,7 @@ const RouteStaticMap = React.createClass({
 
     return(
       <div className="map-thumb">
-        <img width="125" src={'http://maps.googleapis.com/maps/api/staticmap?size=125x125&path=enc:' + this.displayRoute()  + '?key=AIzaSyAqkde8F_7eSorbQZNzN6oVQkvrAHiZCZo' } alt="Run Route Map" />
+        <img width="125" src={'http://maps.googleapis.com/maps/api/staticmap?size=125x125&path=enc:' + this.displayRoute()  + '&key=AIzaSyAqkde8F_7eSorbQZNzN6oVQkvrAHiZCZo' } alt="Run Route Map" />
       </div>
     );}
 
